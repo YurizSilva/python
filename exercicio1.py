493be33709e766d1efc1dce4e84a566b2e98eccf
http.sslbackend <yuri.bhcrisostomo15@gmail.com>
2022-10-25 21:22:52 -0300

    add exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+a = 5
+b = 5.5
+c = "aula"
+d = True
+
+print("O variavel de a é " + str(a))
+print("O variavel de a é " + str(b))
+print("O variavel de a é " + str(c))
+print("O variavel de a é " + str(d))
+
+
