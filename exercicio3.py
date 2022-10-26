423ca8c0dc22927a9091a43b22150a8ca873db35
http.sslbackend <yuri.bhcrisostomo15@gmail.com>
2022-10-26 20:13:23 -0300

    add exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,21 @@
+print("programa para calcular")
+num1 = float(input("Digite o primeiro numero inteiro: "))
+num2 = float(input("Digite o segundo numero inteiro: "))
+
+resultado1 = num1 + num2
+print("o resultado da soma dos dois numeros é: ", resultado1)
+
+resultado2 = num1 - num2
+print("o resultado da subtratação dois numeros é: ", resultado2)
+
+resultado3 = num1 / num2
+print("o resultado da divisão dois numeros é: ", resultado3)
+
+resultado4 = num1 % num2
+print("o resto da divisão dos dois numeros é: ", resultado4)
+
+resultado5 = num1 ** num2
+print("o resultado da potencia dos dois numeros é: ", resultado5)
+
+resultado = num1 * num2
+print("o resultado da multiplicação dos dois numeros é: ", resultado)
