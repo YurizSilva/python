dd36dfbf747da31e415f9d81fa44466871ba8ab8
http.sslbackend <yuri.bhcrisostomo15@gmail.com>
2022-10-26 21:46:50 -0300

    add exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,7 @@
+num1 = float(input("Digite o primeiro numero inteiro que voce deseja: "))
+if (num1 < 10):
+    print ("numero escolhido menor que 10")
+elif (num1 > 10):
+    print ("numero escolhido maior que 10")
+elif (num1 == 10):
+    print ("numero escolhido e igual a 10")
